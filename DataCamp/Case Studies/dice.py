dcdea83c22d8fe06346671306ba38d2a92404bab
Janki Panchal <64837937+jankee31@users.noreply.github.com>
2021-03-18 15:25:54 +0530

    Create dice.py

---
+++ DataCamp/Case Studies/dice.py
@@ -0,0 +1,9 @@
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Using randint() to simulate a dice
+np.random.randint(1,7)
+
+# Using randint() again
+print(print(np.random.randint(1,7)))
