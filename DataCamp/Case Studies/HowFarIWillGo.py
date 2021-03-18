58070150cf8f8fca3370566dc18cddad66e5b51c
Janki Panchal <64837937+jankee31@users.noreply.github.com>
2021-03-18 23:51:56 +0530

    Create HowFarIWillGo.py

---
+++ DataCamp/Case Studies/HowFarIWillGo.py
@@ -0,0 +1,23 @@
+#updated solution of the casestudy 
+
+import numpy as np
+np.random.seed(123)
+
+# Initializing random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: using max so that step can't go below 0
+        step = max(0,step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
